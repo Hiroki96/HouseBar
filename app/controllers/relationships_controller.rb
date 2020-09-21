1c6902ee5b4a81e6157a89c6d9b41c398dfd724f
hiroki96 <hirohhmny1010@gmail.com>
2020-09-21 21:27:13 +0900

    follow ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,16 @@
 class RelationshipsController < ApplicationController
 
-    #フォローする
-    def create
-      current_user.follow(params[:id])
-      redirect_to user_path(params[:id])
-    end
+  def follow
+    @user = User.find_by(id: params[:id])
+    current_user.follow(params[:id])
+    #redirect_to user_path(params[:id])
+  end
 
-    # アンフォローする
-    def destroy
-      current_user.unfollow(params[:id])
-      redirect_to user_path(params[:id])
-    end
+
+  def unfollow
+    @user = User.find_by(id: params[:id])
+    current_user.unfollow(params[:id])
+    #redirect_to user_path(params[:id])
+  end
 
 end
