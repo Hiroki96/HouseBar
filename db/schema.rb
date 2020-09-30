0bb453139fefe76f7505b74e97dc41cee1d102f2
hiroki96 <hirohhmny1010@gmail.com>
2020-09-30 14:53:02 +0900

    change User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_044112) do
+ActiveRecord::Schema.define(version: 2020_09_30_054645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2020_09_29_044112) do
     t.integer "weight"
     t.string "uid"
     t.string "provider"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
