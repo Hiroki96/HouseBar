a3784293d13dc5fffe57280eb78f79d189a78291
hiroki96 <hirohhmny1010@gmail.com>
2020-09-30 14:35:31 +0900

    refactoring

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# README
+# HouseBar
 
 This README would normally document whatever steps are necessary to get the
 application up and running.
+
+
 
 Things you may want to cover:
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_082920) do
+ActiveRecord::Schema.define(version: 2020_09_29_044112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,7 +89,6 @@ ActiveRecord::Schema.define(version: 2020_09_21_082920) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.string "image"
     t.text "profile"
     t.integer "weight"
     t.string "uid"
